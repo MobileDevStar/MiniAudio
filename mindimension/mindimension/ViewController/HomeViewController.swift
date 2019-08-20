bd5db1ff25adf6792dac656251fb63a556edfc3d
ITDevStar <itdevstar@hotmail.com>
2019-08-20 13:00:20 +0300

    main ui structure

---
+++ mindimension/mindimension/ViewController/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  mindimension
+//
+//  Created by simba on 8/20/19.
+//  Copyright © 2019 simba. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var titlebar: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        initView()
+    }
+    
+    func initView() {
+
+    }
+    
+    
+}
